bb1a53c97fa1f67b8e40cedabe60263db9a6ae24
diogo.mota <diogo.mota@dedalus.com>
2026-03-23 22:22:06 +0000

    improved argocd script

---
+++ scripts/argocd-setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Installs Argo CD and applies the root App of Apps manifest.
+
+set -euo pipefail
+
+ARGOCD_VERSION='v3.3.4' # Taken from https://github.com/argoproj/argo-cd/releases
+REPO_URL='https://github.com/diogofrmota/swiss-post-project'
+NAMESPACE='argocd'
+
+echo 'Creating namespace ${NAMESPACE}'
+kubectl create namespace '${NAMESPACE}' --dry-run=client -o yaml | kubectl apply -f -
+
+echo 'Installing Argo CD ${ARGOCD_VERSION}'
+kubectl apply -n '${NAMESPACE}' -f \
+  'https://raw.githubusercontent.com/argoproj/argo-cd/${ARGOCD_VERSION}/manifests/install.yaml'
+
+echo 'Waiting for Argo CD server to be ready...'
+kubectl rollout status deployment/argocd-server -n "${NAMESPACE}" --timeout=120s # Only continues after all pods are updated and ready
+
+echo 'Applying root App of Apps'
+kubectl apply -f gitops-setup/root-app.yaml # Setup Argocd witch will setup the apps (taken from the repo)
+
+echo 'ArgoCD setup complete!'
+echo 'Argo CD initial admin password:'
+kubectl get secret argocd-initial-admin-secret -n argocd \
+  -o jsonpath="{.data.password}" | base64 -d
+echo 'Port-forward:  kubectl port-forward svc/argocd-server -n argocd 8080:443'
+echo 'URL:           https://localhost:8080'
